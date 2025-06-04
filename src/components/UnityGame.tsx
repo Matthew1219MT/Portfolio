d5317f6a06525bf40057ece890f0a636b7e91dca
MatthewTseChunHei <Matttse1219@gmail.com>
2025-06-05 00:22:52 +1000

    Hide Game from Mobile User

--- src/components/UnityGame.tsx
+++ src/components/UnityGame.tsx
@@ -1,21 +1,11 @@
 import { useState } from 'react';
 import './UnityGame.css';
-// import { Unity, useUnityContext } from "react-unity-webgl";
 
 const UnityGame = () => {
-
-    // const { unityProvider, loadingProgression, isLoaded } = useUnityContext({
-    //     loaderUrl: process.env.PUBLIC_URL + "/Build/game/StudyWar/Build/StudyWar.loader.js",
-    //     dataUrl: process.env.PUBLIC_URL + "/Build/game/StudyWar/Build/StudyWar.data.unityweb",
-    //     frameworkUrl: process.env.PUBLIC_URL + "/Build/game/StudyWar/Build/StudyWar.framework.js.unityweb",
-    //     codeUrl: process.env.PUBLIC_URL + "/Build/game/StudyWar/Build/StudyWar.wasm.unityweb"
-    // });
 
     const [isLoaded, setIsLoaded] = useState<boolean>(false);
 
     return <div className='game-container'>
-        {/* {!isLoaded && <p>Loading... {Math.round(loadingProgression * 100)}%</p>}
-        <Unity style={{height: '600px', width: '960px', visibility: isLoaded ? "visible" : "hidden", margin: '0 auto'}}unityProvider={unityProvider}/> */}
         <iframe
             src="https://matthew1219mt.github.io/StudyWar/"
             className='game'
@@ -25,6 +15,7 @@ const UnityGame = () => {
             style={{visibility: isLoaded ? "visible" : "hidden"}}
             onLoad={()=>{setIsLoaded(true)}}
         />
+        <div className='mobile'>You screen is too small, get a larger screen for the game : )</div>
     </div>
 }
 
