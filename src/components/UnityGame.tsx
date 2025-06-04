a973b42fc09aa9a895106db6eefece61dc0f58f6
MatthewTseChunHei <Matttse1219@gmail.com>
2025-06-04 23:48:53 +1000

    Alternate Solution to Game Hosting

--- src/components/UnityGame.tsx
+++ src/components/UnityGame.tsx
@@ -1,18 +1,30 @@
+import { useState } from 'react';
 import './UnityGame.css';
-import { Unity, useUnityContext } from "react-unity-webgl";
+// import { Unity, useUnityContext } from "react-unity-webgl";
 
 const UnityGame = () => {
 
-    const { unityProvider, loadingProgression, isLoaded } = useUnityContext({
-        loaderUrl: "Build/game/StudyWar/Build/StudyWar.loader.js",
-        dataUrl: "Build/game/StudyWar/Build/StudyWar.data.unityweb",
-        frameworkUrl: "Build/game/StudyWar/Build/StudyWar.framework.js.unityweb",
-        codeUrl: "Build/game/StudyWar/Build/StudyWar.wasm.unityweb"
-    });
+    // const { unityProvider, loadingProgression, isLoaded } = useUnityContext({
+    //     loaderUrl: process.env.PUBLIC_URL + "/Build/game/StudyWar/Build/StudyWar.loader.js",
+    //     dataUrl: process.env.PUBLIC_URL + "/Build/game/StudyWar/Build/StudyWar.data.unityweb",
+    //     frameworkUrl: process.env.PUBLIC_URL + "/Build/game/StudyWar/Build/StudyWar.framework.js.unityweb",
+    //     codeUrl: process.env.PUBLIC_URL + "/Build/game/StudyWar/Build/StudyWar.wasm.unityweb"
+    // });
+
+    const [isLoaded, setIsLoaded] = useState<boolean>(false);
 
     return <div className='game-container'>
-        {!isLoaded && <p>Loading... {Math.round(loadingProgression * 100)}%</p>}
-        <Unity style={{height: '600px', width: '960px', visibility: isLoaded ? "visible" : "hidden", margin: '0 auto'}}unityProvider={unityProvider}/>
+        {/* {!isLoaded && <p>Loading... {Math.round(loadingProgression * 100)}%</p>}
+        <Unity style={{height: '600px', width: '960px', visibility: isLoaded ? "visible" : "hidden", margin: '0 auto'}}unityProvider={unityProvider}/> */}
+        <iframe
+            src="https://matthew1219mt.github.io/StudyWar/"
+            className='game'
+            title="StudyWar"
+            width="1010"
+            height="650"
+            style={{visibility: isLoaded ? "visible" : "hidden"}}
+            onLoad={()=>{setIsLoaded(true)}}
+        />
     </div>
 }
 
