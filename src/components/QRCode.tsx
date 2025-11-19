213e24af691c757c3569140920f52ee2b78d8109
Matthew T <matttse1219@gmail.com>
2025-11-19 13:24:02 +1100

    QR Code Display PC Debug

--- src/components/QRCode.tsx
+++ src/components/QRCode.tsx
@@ -3,6 +3,7 @@ import './QRCode.css';
 const QRCode = () => {
     return (
         <div className="QR-code-container">
+            Scan me to see this portfolio !!!
             <img src={`${process.env.PUBLIC_URL}/resources/QRCode.png`} alt="QR code of portfolio"></img>
         </div>
     );
